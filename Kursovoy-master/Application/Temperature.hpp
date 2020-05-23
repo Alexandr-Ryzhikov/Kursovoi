d8f843922d9e897fe317e9740cb9677dc10cf26e
Alexandr-Ryzhikov <raycalmar@gmail.com>
2020-05-23 11:41:33 +0500

    78

--- Kursovoy-master/Application/Temperature.hpp
+++ Kursovoy-master/Application/Temperature.hpp
@@ -1,22 +1,22 @@
 #pragma once
 #include "IUnits.hpp"
-
+#include "IVariable.hpp"
 #include <array>
 using namespace std;
 
 class Temperature : public IVariable
 {
 private:
-  float Value;
   float Temper;
   std::array <IUnits*, 3> units;
   //std::uint8_t CurrentUnits;
-  float k = (3.3f/(4096.0f*0.0025f));
-  float b = (25.0f-0.76f/0.0025f);
+  //float k = (3.3f/(4096.0f*0.0025f));
+  //float b = (25.0f-0.76f/0.0025f);
   constexpr SusuStringView Name();
   int CurrentIndex = 0;
 public:
-
+  using IVariable::IVariable;
+  
   void NextUnits()
   {
       for(int i=0; i<2; i++)
@@ -34,7 +34,12 @@ public:
     Temper = Value;
   }
   
-  void GetValueAndName()
+    float GetValue() override
+  {
+    return Value;
+  }
+  
+  void GetValueAndName() override
   {
     //units[currentIndex].Calculate() ;
     auto Value = units[CurrentIndex]->GetValue();
--- Kursovoy-master/Application/VariableTask.hpp
+++ Kursovoy-master/Application/VariableTask.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Voltage.hpp"
+#include "Temperature.hpp"
 #include "thread.hpp"
 #include "event.hpp"
 #include "ADC.hpp" //for Resolution, tSampleRate
@@ -10,8 +11,9 @@ template <typename myADC>
 class VariableTask : public OsWrapper::Thread<512> 
 {
 private:
-  Voltage VoltageValue=Voltage(1, 2);//(3.3F/1024)(25.0F - 0.76F/0.0025F)
-  float TemperatureValue;
+  Voltage VoltageValue=Voltage((3.3F/1024), (25.0F - 0.76F/0.0025F));//(3.3F/1024)(25.0F - 0.76F/0.0025F)
+  Temperature TemperatureValue=Temperature((3.3f/(4096.0f*0.0025f)),(25.0f-0.76f/0.0025f));
+  
   
   OsWrapper::Event& myEvent;  
 
@@ -19,6 +21,8 @@ private:
 public:
   void Execute() override
   {
+    //Koldunstvo s ADC//////////////////////////////////////////////////////////////////////////////////////////
+    
     myADC::On(); //vkluchaen adc
     myADC::adcConfig(Resolution::Bits12, tSampleRate::Cycles480, tSampleRate::Cycles480); //nastraivaem adc
     myADC::SetChannels(0, 18); //podkluchaem kanali
@@ -26,20 +30,24 @@ public:
     
    for( ; ;)
   {
-    //Koldunstvo s ADC
-    
     auto codes = myADC::GetValue(); //po idee, uzhe ne nuzhen
     
-    //Koldunstvo s IVariable(poluchaem temperature and voltage)
+    //Koldunstvo s Voltage(poluchaem voltage)////////////////////////////////////////////////////////////////////
+    
     VoltageValue.Calculation(codes[0]); //chitaem
     VoltageValue.GetValueAndName();
     //TemperatureValue =  tuple_;//poluchaem temperaturu
     //VoltageValue = IVariable::GetValue(); //poluchaem voltage
     
-    //Vivodim chernila
+    //Koldunstvo s Temperature(poluchaem temperature)////////////////////////////////////////////////////////////
+    TemperatureValue.Calculation(codes[1]);
+    TemperatureValue.GetValueAndName();
+    
+    //Vivodim chernila///////////////////////////////////////////////////////////////////////////////////////////
+    
     //std::cout << "Temperature: " << std::get<0>(TemperatureValue) << std::get<1>(TemperatureValue) << std::endl;
     std::cout << "Voltage: " << VoltageValue.GetValue() << " V. " << std::endl;
-     
+    std::cout << "Temperature: " << TemperatureValue.GetValue() << std::endl;
     //Koldunstvo s knopkoi
     Sleep(500ms);
     if (myEvent.Wait() !=0)
